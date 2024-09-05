3a845e4a41ece34bb3562ff06d04675a41b3d8ec
YouHaveToDo <zmfpdkwl2@gmail.com>
2024-09-05 14:13:33 +0900

    feat: 시간도 선택할 수 있도록 UI 및 로직 추가

--- scripts/content.js
+++ scripts/content.js
@@ -78,7 +78,7 @@ async function reservation() {
             );
         });
 
-        const time = document.querySelector(".td-title label")
+        const time = document.querySelector(`.td-title label[for='rbTime-${options.time}']`)
         const type = document.querySelector("label[for='userType-P']")
         const count = document.querySelector("#headcount")
         const name = document.querySelector("#user2")
